47425beb5d5900de924006f34689d7a25ba2722b
naisenyamungai <naisenyamungai@gmail.com>
2019-06-06 16:02:00 +0300

    add styling for index.hbs

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import static spark.Spark.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.sql2o.Sql2o;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/", (request, response) -> {
+                    Map<String, Object> model = new HashMap<>();
+                    return new ModelAndView(model, "index.hbs");
+                }, new HandlebarsTemplateEngine()
+        );
+
+    }
+}
