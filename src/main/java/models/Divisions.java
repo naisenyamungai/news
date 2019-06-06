52ec833469855a46ab487537befe8d7a601e1303
naisenyamungai <naisenyamungai@gmail.com>
2019-06-06 20:04:11 +0300

    create departments, sections and employees classes

--- src/main/java/models/Divisions.java
+++ src/main/java/models/Divisions.java
@@ -1,4 +1,9 @@
 package models;
 
+import java.util.Objects;
+import java.util.ArrayList;
+
 public class Divisions {
+    private String division_name;
+    private ArrayList<Departments> departments
 }
