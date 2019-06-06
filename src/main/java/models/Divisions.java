7d8d0b8307de85e372cadb539b29de20f2f35f33
naisenyamungai <naisenyamungai@gmail.com>
2019-06-06 20:20:16 +0300

    create directors and hod classes

--- src/main/java/models/Divisions.java
+++ src/main/java/models/Divisions.java
@@ -5,5 +5,45 @@ import java.util.ArrayList;
 
 public class Divisions {
     private String division_name;
-    private ArrayList<Departments> departments
+    private int id;
+    private ArrayList<Directors> directors;
+    private ArrayList<Departments> departments;
+
+    public Divisions(String division_name){
+        this.division_name = division_name;
+    }
+
+    public void setDivision_name(String division_name){
+        this.division_name = division_name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getDivision_name(){
+        return division_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Divisions divisions = (Divisions) o;
+        return getId() == divisions.getId() &&
+                Objects.equals(getDivision_name(), divisions.getDivision_name()) &&
+                Objects.equals(directors, divisions.directors) &&
+                Objects.equals(departments, divisions.departments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDivision_name(), getId());
+    }
+
 }
