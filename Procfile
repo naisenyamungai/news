137c9c9d02a8fe5605cba7bbefb33bdf657d7a1d
naisenyamungai <naisenyamungai@gmail.com>
2019-06-07 17:31:34 +0300

    create procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./build/install/news/bin/news
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,18 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import dao.Sql2oDepartmentDao;
-import dao.Sql2oDivisionDao;
-import dao.Sql2oDirectorDao;
-import dao.Sql2oHodDao;
-import dao.Sql2oSectionDao;
-import dao.Sql2oEmployeeDao;
+import com.google.gson.Gson;
+import dao.*;
 import models.DB;
 import models.Division;
 import models.Director;
 import models.Department;
 import models.Hod;
 import models.Section;
+import models.Article;
+import models.Classified;
 import models.Employee;
 import org.sql2o.Sql2o;
 import spark.ModelAndView;
@@ -32,7 +30,9 @@ public class App {
         Sql2oHodDao hodDao = new Sql2oHodDao(sql2o);
         Sql2oSectionDao sectionDao = new Sql2oSectionDao(sql2o);
         Sql2oEmployeeDao employeeDao = new Sql2oEmployeeDao(sql2o);
-
+        Sql2oArticleDao articleDao = new Sql2oArticleDao(sql2o);
+        Sql2oClassifiedDao classifiedDao = new Sql2oClassifiedDao(sql2o);
+        Gson gson = new Gson();
 
         get("/", (request, response) -> {
                     Map<String, Object> model = new HashMap<>();
@@ -170,5 +170,65 @@ public class App {
         }, new HandlebarsTemplateEngine());
 
 
+
+        //NEWS SECTION
+
+        //get: show all articles
+        get("/article", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Article> allArticles = articleDao.getAll();
+            model.put("articles", allArticles);
+            return new ModelAndView(model, "article.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //get: show new article form
+        get("/articles/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Article> articles = articleDao.getAll();
+            return new ModelAndView(model, "articleform.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+        //division: process new division form
+        post("/articles", (req, res) -> { //URL to make new task on POST route
+            Map<String, Object> model = new HashMap<>();
+            String title = req.queryParams("title");
+            String description = req.queryParams("description");
+            String story = req.queryParams("story");
+            Article newArticle = new Article(title, description, story);
+            articleDao.add(newArticle);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+
+        get("/articles/{{id}}", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Article> allArticles = articleDao.getAll();
+            model.put("articles", allArticles);
+            return new ModelAndView(model, "articleDetails.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+  // API SECTION
+
+        post("/divisions/api/new", "application/json", (req, res) -> {
+            Division division = gson.fromJson(req.body(), Division.class);
+            divisionDao.add(division);
+            res.status(201);;
+            return gson.toJson(division);
+        });
+
+        //READ
+        get("/divisions/api", "application/json", (req, res) -> {
+            return gson.toJson(divisionDao.getAll());
+        });
+
+        get("/divisions/api/:id", "application/json", (req, res) -> {
+            int divisionId = Integer.parseInt(req.params("id"));
+            return gson.toJson(divisionDao.findById(divisionId));
+        });
     }
 }
