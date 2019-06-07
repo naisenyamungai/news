cc8c8a7cdf33debdee7b7d2aac839b03bc2d8421
naisenyamungai <naisenyamungai@gmail.com>
2019-06-07 14:39:31 +0300

    add logic to dao files

--- src/main/java/dao/ArticleDao.java
+++ src/main/java/dao/ArticleDao.java
@@ -1,4 +1,21 @@
 package dao;
+import models.Article;
+import java.util.List;
 
 public interface ArticleDao {
+    List<Article> getAll();
+
+    void add(Article article);
+
+    Article findById(int id);
+
+    void update(int id, String title, String description);
+
+    void deleteById(int id);
+
+    void clearAllArticles();
 }
+
+
+
+
--- src/main/java/dao/ClassifiedDao.java
+++ src/main/java/dao/ClassifiedDao.java
@@ -1,4 +1,21 @@
 package dao;
+import models.Classified;
+import java.util.List;
 
 public interface ClassifiedDao {
+    List<Classified> getAll();
+
+    void add(Classified classified);
+
+    Classified findById(int id);
+
+    void update(int id, String title, String description);
+
+    void deleteById(int id);
+
+    void clearAllClassifieds();
 }
+
+
+
+
